64545f8dcb077191c4e4e609add6ac3d1fc3bfb3
gisaopera <104257643+gisaopera@users.noreply.github.com>
2022-06-26 22:29:10 +0200

    Ispravka assert poruka2.

--- src/test/java/tests/LoyaltyCardTest.java
+++ src/test/java/tests/LoyaltyCardTest.java
@@ -19,7 +19,7 @@ public class LoyaltyCardTest {
             homePage.clickLoyaltyTab();
 
             LoyaltyCardPage loyaltyCardPage = new LoyaltyCardPage(driver);
-            String LoyaltyText = "Ukoliko ste u poslednjih 365 dana imali kupovinu u ukupnom iznosu od 40.000 dinara.";
+            String LoyaltyText = "Ukoliko ste u poslednjih 365 dana imali kupovinu u ukupnom iznosu od 40.000 dinara";
             String actualLoyaltyText = loyaltyCardPage.getLoyaltyText();
             assert actualLoyaltyText.contains(LoyaltyText) : "Error. Wrong text found. Expected: " + LoyaltyText + ". Actual: " + actualLoyaltyText;
 
